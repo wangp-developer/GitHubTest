14c45e739da7b92dead3f3a04a94d0001021df6e
wangp-developer <37281777+wangp-developer@users.noreply.github.com>
2018-03-26 18:44:56 +0800

    测试项目TestGit

---
+++ TestDemo/TestDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestDemo
+//
+//  Created by DHgate on 26/3/18.
+//  Copyright © 2018年 DHgate. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestDemo/TestDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestDemo
+//
+//  Created by DHgate on 26/3/18.
+//  Copyright © 2018年 DHgate. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
