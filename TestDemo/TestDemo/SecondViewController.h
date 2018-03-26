6602f1ac7f775d0030084165e85a183371c4f4a5
wangp-developer <37281777+wangp-developer@users.noreply.github.com>
2018-03-26 19:03:14 +0800

    添加SecondViewController类以及修改进入APP的方式

---
+++ TestDemo/TestDemo/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  TestDemo
+//
+//  Created by DHgate on 26/3/18.
+//  Copyright © 2018年 DHgate. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
--- TestDemo/TestDemo/ViewController.m
+++ TestDemo/TestDemo/ViewController.m
@@ -7,7 +7,7 @@
 //
 
 #import "ViewController.h"
-
+#import "SecondViewController.h"
 @interface ViewController ()
 
 @end
@@ -17,12 +17,25 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-    self.view.backgroundColor = [UIColor redColor];
+    self.view.backgroundColor = [UIColor whiteColor];
     UIView *baseView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, 100, 100)];
     baseView.backgroundColor = [UIColor greenColor];
     [self.view addSubview:baseView];
+    
+    UIButton *btn = [UIButton buttonWithType:UIButtonTypeCustom];
+    [self.view addSubview:btn];
+    [btn setTitle:@"点击" forState:UIControlStateNormal];
+    [btn setTitleColor:[UIColor redColor] forState:UIControlStateNormal];
+    btn.frame = CGRectMake((self.view.bounds.size.width - 100) / 2, (self.view.bounds.size.height - 100) / 2, 100, 100);
+    [btn addTarget:self action:@selector(clickAction:) forControlEvents:UIControlEventTouchUpInside];
 }
 
+- (IBAction)clickAction:(id)sender {
+    
+    SecondViewController *seVC = [[SecondViewController alloc] init];
+    [self.navigationController pushViewController:seVC animated:YES];
+    
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
